0727d96a1a5c89990ff880138ebbdb387adfdb34
Dean Gilroy <dean.gilroy@gmail.com>
2016-02-07 10:18:48 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// $( document ).ready(function() {
+//     console.log( "All DOM elements loaded!" );
+
+//     $('.beer-btn').click(function (event) {
+//     	event.preventDefault();
+
+//     	$('.beers').show();
+//     });
+
+// });
